431ec81e2bc6a6d3b7a47156a3c09f83f53180d8
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-02-11 20:13:42 +0200

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22
+{
+    internal class Calculate
+    {
+        /// <summary>
+        /// Поле 1
+        /// </summary>
+        public double first;
+
+        /// <summary>
+        /// Поле 2
+        /// </summary>
+        public double second;
+
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public Calculate() { }
+
+        /// <summary>
+        /// Конструктор с одним параметром.
+        /// </summary>
+        /// <param name="first"></param>
+        public Calculate(double first)
+        {
+            this.first = first;
+        }
+
+        /// <summary>
+        /// Конструктор с двумя параметрами.
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        public Calculate(double first, double second) : this(first)
+        {
+            this.second = second;
+        }
+
+        /// <summary>
+        /// Метод, преобразующий информацию об объекте в строку.
+        /// </summary>
+        /// <returns>Строку с информацией об объекте.</returns>
+        public override string ToString()
+        {
+            return $"Поле 1: {first}\tПоле 2: {second}";
+        }
+
+        /// <summary>
+        /// Метод, вычисляющий квадратный корень из произведения значения полей first и second.
+        /// </summary>
+        /// <returns>Квадратный корень из произведения значения полей first и second</returns>
+        public double HandleFields()
+        {
+            return Math.Sqrt(first * second);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта класс с использованием
+            // конструктора без параметров и присваивания значений полям объекта.
+            Calculate calc1 = new Calculate();
+            calc1.first = 1.5;
+            calc1.second = 0.5;
+            Console.WriteLine($"{calc1}\n" +
+                $"Результат обработки значений полей: {calc1.HandleFields():F2}\n");
+
+            // Создание объекта класс с использованием явной инициализации.
+            Calculate calc2 = new Calculate() { first = 3, second = 4 };
+            Console.WriteLine($"{calc2}\n" +
+                $"Результат обработки значений полей: {calc2.HandleFields():F2}\n");
+
+            // Создание объекта класс  с использованием конструктора с двумя параметрами,
+            // введенными с клавиатуры.
+            Console.Write("Поле 1: ");
+            double field1 = double.Parse(Console.ReadLine());
+            Console.Write("Поле 2: ");
+            double field2 = double.Parse(Console.ReadLine());
+            Calculate calc3 = new Calculate(field1, field2);
+            Console.WriteLine($"{calc3}\n" +
+                $"Результат обработки значений полей: {calc3.HandleFields():F2}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
